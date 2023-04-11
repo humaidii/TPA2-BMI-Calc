6252015e0919959bf4cba2cde6ee47997de3673e
Humaidi Hambali <aliesp241@gmail.com>
2023-04-11 23:01:16 +0700

    add readme page

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# TPA2 - BMI CALCULATOR
+
+This calculator was created to complete the second task of a technical project assignment. This task uses DOM to validate if the input is a number or string, is a valued or null, is greater than 0, or is less than 0. 
+## Acknowledgements
+
+ - [Place Where I Learn JavaScript DOMs](https://https://javascripttutorial.net/javascript-dom)
+ - [Place When I Got An Headache](https://w3schools.com)
+ - [Make a Readme Easier](https://readme.so)
+
+
+## Categories
+- Underweight = <18.5
+- Normal weight = 18.5 – 24.9
+- Overweight = 25 – 29.9
+- Obesity = BMI of 30 or greater
+
+## Feedback
+
+If you have any feedback, please reach out to us at aliesp241.gmail.com
+
+
+## 🚀 About Me
+I'm a back end web developer wannabe....
+
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener('click', () => {
     const result = document.getElementById('output');
     let height_status=false, weight_status=false;
 
-    
+
 
     if(height === '' || isNaN(height) || (height <= 0)){
         document.getElementById('height_error').innerHTML = 'masukkan angka';
@@ -29,11 +29,11 @@ button.addEventListener('click', () => {
     if(height_status && weight_status){
         const bmi = (weight / ((height*height)/10000)).toFixed(1);
 
-        if(bmi < 18.6){
+        if(bmi < 18.5){
             result.innerHTML = 'Your  BMI  is  ' + bmi + '  which  means  you  are  Under  Weight';
-        }else if(bmi >= 18.5 && bmi < 24.9){
+        }else if(bmi >= 18.5 && bmi <= 24.9){
             result.innerHTML = 'Your  BMI  is  ' + bmi + '  which  means  you  are  Normal';
-        }else if(bmi >= 25 && bmi < 29.9){
+        }else if(bmi >= 25 && bmi <= 29.9){
             result.innerHTML = 'Your  BMI  is  ' + bmi + '  which  means  you  are  Over  Weight';
         }else{
             result.innerHTML = 'Your  BMI  is  ' + bmi + '  which  means  you  are  Obesity';
